16a7f75f28fb18dea7dc6730fa5191f0c2a97dae
dittops <dittops@gmail.com>
2024-09-29 19:08:21 +0000

    fxi: requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
+import os
 from setuptools import find_packages, setup
+from typing import Dict, List
 
+ROOT_DIR = os.path.dirname(__file__)
+
+def get_path(*filepath) -> str:
+    return os.path.join(ROOT_DIR, *filepath)
+
+def _read_requirements(filename: str) -> List[str]:
+    with open(get_path(filename)) as f:
+        requirements = f.read().strip().split("\n")
+    resolved_requirements = []
+    for line in requirements:
+        if line.startswith("-r "):
+            resolved_requirements += _read_requirements(line.split()[1])
+        elif line.startswith("--"):
+            continue
+        else:
+            resolved_requirements.append(line)
+    print(resolved_requirements)
+    return resolved_requirements
 
 setup(
     author="Bud Ecosystem",
@@ -9,6 +29,6 @@ setup(
     keywords="bud",
     name="llm_benchmark",
     packages=find_packages(include=["llm_benchmark", "llm_benchmark.*"]),
-    install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=_read_requirements('requirements.txt'),
     version="0.0.2",
 )
